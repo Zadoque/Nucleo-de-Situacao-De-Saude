39c5c71dd1f2b7fdfe4d8b23321a9a12957d64bf
Zadoque Carneiro <139159544+Zadoque@users.noreply.github.com>
2026-08-28 10:47:34 -0300

    Add placeholder flake.json (to be renamed to flake.nix manually)

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  "description": "Núcleo de Situação de Saúde (NSS)",
+  "inputs": {
+    "nixpkgs": {
+      "url": "github:NixOS/nixpkgs/nixos-24.05"
+    }
+  },
+  "outputs": {
+    "defaultPackage": {
+      "type": "package",
+      "path": "."
+    }
+  }
+}
